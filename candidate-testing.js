06e76718ea6e1898403e2141430c4a0b7ed9b71a
DeMarcus Benion <dbenion9@gmail.com>
2024-06-24 19:05:39 -0500

    Task 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,9 +5,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
@@ -18,18 +18,25 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  let candidateName = prompt("Please enter your name:");
+  console.log("Candidate's name is:" + candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = prompt("Who was the first American woman in space?");
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  if (candidateAnswer === correctAnswer) {
+    console.log("That's correct, " + candidateName + "! Well done.");
+} else {
+    console.log("Sorry, " + candidateName + ". The correct answer is " + correctAnswer + ".");
+}
+
 
 
 
